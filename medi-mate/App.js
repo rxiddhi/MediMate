71d47d47053e644984d6bb9420ffcfa53acd80e9
Riddhi <riddhi.khera.07@gmail.com>
2025-09-19 17:22:54 +0530

    Update App.js

--- medi-mate/App.js
+++ medi-mate/App.js
@@ -1,55 +1,11 @@
-import React, { useEffect } from "react";
-import { Platform, LogBox } from "react-native";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import HomeScreen from "./screens/HomeScreen";
-import AddMedicineScreen from "./screens/AddMedicineScreen";
-import ProfileScreen from "./screens/ProfileScreen";
-import * as Notifications from "expo-notifications";
-import * as Device from "expo-device";
-
-LogBox.ignoreLogs(["Setting a timer"]); 
-
-const Tab = createBottomTabNavigator();
-
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: true,
-    shouldSetBadge: false,
-  }),
-});
+import TabNavigator from "./navigation/TabNavigator";
 
 export default function App() {
-  useEffect(() => {
-    async function registerForPushNotificationsAsync() {
-      if (!Device.isDevice) {
-        console.warn(
-          "Must use physical device for notifications (Expo Go on phone works)."
-        );
-        return;
-      }
-      const { status: existingStatus } =
-        await Notifications.getPermissionsAsync();
-      let finalStatus = existingStatus;
-      if (existingStatus !== "granted") {
-        const { status } = await Notifications.requestPermissionsAsync();
-        finalStatus = status;
-      }
-      if (finalStatus !== "granted") {
-        console.warn("Permission not granted for notifications!");
-      }
-    }
-    registerForPushNotificationsAsync();
-  }, []);
-
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Add" component={AddMedicineScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
+      <TabNavigator />
     </NavigationContainer>
   );
 }

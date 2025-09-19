e6620c46720a3faa6a7f80085a3f6e846a75bf77
Riddhi <rxiddhi@gmail.com>
2025-09-19 17:12:04 +0530

    screens and basic page

--- medi-mate/App.js
+++ medi-mate/App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { Platform, LogBox } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./screens/HomeScreen";
+import AddMedicineScreen from "./screens/AddMedicineScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import * as Notifications from "expo-notifications";
+import * as Device from "expo-device";
+
+LogBox.ignoreLogs(["Setting a timer"]); 
+
+const Tab = createBottomTabNavigator();
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
+  useEffect(() => {
+    async function registerForPushNotificationsAsync() {
+      if (!Device.isDevice) {
+        console.warn(
+          "Must use physical device for notifications (Expo Go on phone works)."
+        );
+        return;
+      }
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        console.warn("Permission not granted for notifications!");
+      }
+    }
+    registerForPushNotificationsAsync();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Add" component={AddMedicineScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ medi-mate/screens/AddMedicineScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function AddMedicineScreen() {
+  return (
+    <View>
+      <Text>Add Medicine Screen</Text>
+    </View>
+  );
+}
